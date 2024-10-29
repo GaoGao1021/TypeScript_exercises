f0f49c86087c2116d7bf3f6f9ad9371cb880d98b
GaoGao1021 <beans1412love@gmail.com>
2024-10-29 21:40:32 +0800

    Q1.js

---
+++ Q1.js
@@ -0,0 +1,6 @@
+var Age = 25;
+Age = 30;
+var Name = "Alice";
+Name = "Bob";
+console.log("Age: ".concat(Age));
+console.log("Name: ".concat(Name));
---
+++ Q1.ts
@@ -0,0 +1,8 @@
+const Age: number = 25;
+Age = 30;
+
+let Name: string = "Alice";
+Name = "Bob";
+
+console.log(`Age: ${Age}`);
+console.log(`Name: ${Name}`);
